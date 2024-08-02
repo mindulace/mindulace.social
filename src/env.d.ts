3b07425114f091d6e6c27d74095e8ece156b5d7f
Mindulace <hendrik@mindu.cloud>
2024-08-02 22:20:08 +0200

    add directus url and cf access env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly DIRECTUS_URL: string;
+    readonly CF_ACCESS_CLIENT_ID: string;
+    readonly CF_ACCESS_CLIENT_SECRET: string;
+}
